23928f7f92aee3845d2251454a74edb20e645be0
PritishWadhwa <pritish19440@iiitd.ac.in>
2022-07-26 17:58:54 +0530

    Codefiles updated

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="lawsplit",                             # This is the name of the package
-    version="0.0.1",                             # The initial release version
-    author="Pritish Wadhwa",                     # Full name of the author
-    description="LawSplit: A smart legal text sentence splitter",
-    # Long description read from the the readme file
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    # List of all python modules to be installed
-    packages=setuptools.find_packages(),
+from distutils.core import setup
+setup(
+    name='lawsplit',
+    packages=['lawsplit'],
+    version='1.0.1',
+    license='MIT',
+    description='LawSplit: A smart legal text sentence splitter',
+    author='Pritish Wadhwa',
+    author_email='pritish19440@iiitd.ac.in',
+    url='https://github.com/PritishWadhwa',
+    download_url='https://github.com/PritishWadhwa/LawSplit/archive/refs/tags/v1.0.1.tar.gz',
+    keywords=['LEGAL', 'SPLIT', 'PARAGRAPH', 'DOCUMENTS', 'NLP', 'SENTENCES'],
+    install_requires=[
+        'pysbd'
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],                                      # Information to filter the project on PyPi website
-    python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["lawsplit"],                # Name of the python package
-    # Directory of the source code of the package
-    package_dir={'': 'lawsplit/src'},
-    # Install other dependencies if any
-    install_requires=["pysbd", "re"]
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
+'+
